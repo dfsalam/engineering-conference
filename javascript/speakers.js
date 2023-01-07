00c61f0c8512cf42e5034e1cf07c2c8832e902e8
dfsalam <dfsalam@gmail.com>
2023-01-07 17:37:07 -0500

    Change ../ for ./ inside JS file to see properly the speakers photos

--- javascript/speakers.js
+++ javascript/speakers.js
@@ -3,38 +3,38 @@ const speakers = [
     name: 'Robert Goddard',
     title: 'Physicist at Worcester Polytechnic Institute',
     paragraph: "American engineer, professor, physicist, and inventor who is credited with creating and building the world's first liquid-fueled rocket.",
-    image: '../images/robert_goddard.png',
+    image: './images/robert_goddard.png',
 
   },
   {
     name: 'Mary Jackson',
     title: ' Mathematician and Aerospace engineer at (NACA)',
     paragraph: 'She worked to help women and other minorities to advance their careers, including advising them how to study in order to qualify for promotions. She participated in Project Mercury helping to do complex calculations without computers.',
-    image: '../images/mary_jackson.png',
+    image: './images/mary_jackson.png',
   },
   {
     name: 'Sergei Korolev',
     title: 'Rocket engineer, Chief Designer of the Soviet space program',
     paragraph: 'He was involved in the development of the R-7 Rocket, Sputnik 1, launching Laika, Sputnik 3, the first human-made object to make contact with another celestial body, Belka and Strelka, the first human being, Yuri Gagarin, into space, Voskhod 1, and the first person, Alexei Leonov, to conduct a spacewalk.',
-    image: '../images/sergei_korolev.png',
+    image: './images/sergei_korolev.png',
   },
   {
     name: 'Wernher Von Braun',
     title: 'Chief architect of the Apollo Saturn V rocket',
     paragraph: 'Von Braun is widely seen as the "father of space travel",the "father of rocket science" or the "father of the American lunar program".',
-    image: '../images/wernher_von_braun.png',
+    image: './images/wernher_von_braun.png',
   },
   {
     name: 'Elon Musk',
     title: 'Founder, CEO and chief engineer of SpaceX',
     paragraph: 'His company manufactures the Falcon 9, Falcon Heavy, and Starship launch vehicles, several rocket engines, Cargo Dragon and Crew Dragon spacecraft, and Starlink communications satellites.',
-    image: '../images/elon_musk.png',
+    image: './images/elon_musk.png',
   },
   {
     name: 'Tom Mueller',
     title: 'Founder, CEO, of Impulse Space',
     paragraph: "He is considered one of the world's leading spacecraft propulsion experts and holds several United States patents for propulsion technology.",
-    image: '../images/tom_mueller.png',
+    image: './images/tom_mueller.png',
   },
 ];
 function loadElements() {
@@ -72,7 +72,7 @@ function loadElements() {
     if (count === 2) {
       content += `
     <h4 class="speaker__more desktop__hide">MORE 
-    <img src='../images/arrow_down.png' alt='arrow down'>
+    <img src='./images/arrow_down.png' alt='arrow down'>
     </h4>
   `;
     }
@@ -81,7 +81,7 @@ function loadElements() {
   // Add the LESS button
   content += `
 <h4 class="speaker__less hide desktop__hide">LESS
-<img src='../images/arrow_up.png' alt='arrow up'>
+<img src='./images/arrow_up.png' alt='arrow up'>
 </h4>
 `;
 
